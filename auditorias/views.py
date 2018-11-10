2680d834b49699d7353741a9ac0294810950137b
matias <m.tejosbucarey@uandresbello.edu>
2018-11-09 23:57:00 -0300

    adelanto 2

--- auditorias/views.py
+++ auditorias/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from retro_auth.models  import *
+from retro.models import *
 
 # Create your views here.
 
 def auditorias(request):
-    return render(request, "auditorias/auditorias.html", {"umbral":UserProfile.objects.all()})
+    return render(request, "auditorias/auditorias.html", {"umbral":UserProfile.objects.all(), "date":Post.objects.all()})
