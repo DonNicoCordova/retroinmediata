1880abaa7bb41b463fca5c2930647f3e675a8b62
jdecomba <jdecomba@gmail.com>
2018-11-10 00:48:02 -0300

    Initial Commit Added Comment Ratings Added Thread Ratings

--- retro/urls.py
+++ retro/urls.py
@@ -4,4 +4,6 @@ from retro import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('Seccion/<int:pk>/', views.section_details, name="section_details"),
+	path('question/', views.question, name="question"),
+    path('forum/', views.forum, name="forum"),
 ]
