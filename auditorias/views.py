193f5c00174efae286793932beebfafd3913c57e
matias <m.tejosbucarey@uandresbello.edu>
2018-11-11 15:59:20 -0300

    fecha actual

--- auditorias/views.py
+++ auditorias/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from retro_auth.models  import *
 from retro.models import *
+import datetime
 
 # Create your views here.
 
 def auditorias(request):
-    return render(request, "auditorias/auditorias.html", {"umbral":UserProfile.objects.all(), "date":Post.objects.all()})
+    # mydate = datetime.datetime.now()
+    return render(request, "auditorias/auditorias.html", {"umbral":UserProfile.objects.all(), "date":Post.objects.all(), "now": datetime.datetime.now()})
+
+def is_past_due(request):
+    pass
