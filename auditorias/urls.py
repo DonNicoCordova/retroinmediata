fae11e5679e3fe0900d5eafe8aa448f69f4cd10e
matias <m.tejosbucarey@uandresbello.edu>
2018-11-13 19:41:22 -0300

    cambiar umbral correccion

--- auditorias/urls.py
+++ auditorias/urls.py
@@ -5,6 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.auditorias, name="auditorias"),
     path('editUmbral/', views.edit_Umbral, name="edit_Umbral"),
-    path('is_teacher/', views.is_teacher, name='is_teacher'),
-    path('auditorias_aut', views.auditorias_aut, name='auditorias_aut')
 ]
