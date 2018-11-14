e81442987af0f54ba82ada4ffcad5430543f3d96
Nicolas Cordova <n.cordovapeaylillo@uandresbello.edu>
2018-11-13 22:52:02 -0300

    agregadas migraciones

--- retro_auth/models.py
+++ retro_auth/models.py
@@ -7,10 +7,10 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User,null=True,default=None,on_delete=models.CASCADE)
     rut = models.CharField(max_length=12, default="Sin Rut")
-    teacher = models.BooleanField()
-    student = models.BooleanField()
-    sacademic = models.BooleanField()
-    dcareer = models.BooleanField()
-    
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=True)
+    is_sacademic = models.BooleanField(default=False)
+    is_dcareer = models.BooleanField(default=False)
+
     def __str__(self):
         return "%s %s Rut: %s" % (self.user.first_name, self.user.last_name, self.rut)
