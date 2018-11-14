36700387fee1ebb22c48b7d4cec6073ddc54219d
Nicolas Cordova <n.cordovapeaylillo@uandresbello.edu>
2018-11-13 22:46:32 -0300

    arreglos al modelo

--- retro_auth/models.py
+++ retro_auth/models.py
@@ -7,6 +7,10 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User,null=True,default=None,on_delete=models.CASCADE)
     rut = models.CharField(max_length=12, default="Sin Rut")
+    teacher = models.BooleanField()
+    student = models.BooleanField()
+    sacademic = models.BooleanField()
+    dcareer = models.BooleanField()
     
     def __str__(self):
         return "%s %s Rut: %s" % (self.user.first_name, self.user.last_name, self.rut)
