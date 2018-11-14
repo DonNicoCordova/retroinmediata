4e7c9295262c6f42c63513c9de9c323459cf5f26
liapino <l.ancavilpino@gmail.com>
2018-11-13 22:30:14 -0300

    Agregar minuta v.1.1

--- auditorias/views.py
+++ auditorias/views.py
@@ -9,13 +9,6 @@ from auditorias.models import *
 def auditorias(request):
     pass
 
-"""def usuarios_registrados(request):
-    lista_registrados = []
-    for i in User.objects.all():
-        lista_registrados.append(i)
-    print(len(lista_registrados))"""
-
-
 def historial_auditorias(request):
     template_name = "auditorias/_auditorias.html"
     data = {}
@@ -29,93 +22,3 @@ def buscar_auditorias(request):
     data['auditorias'] = ForoAudit.objects.all()
     
     return render(request, template_name, data)
-
-"""
-#def auditorias(request):
-#    # mydate = datetime.datetime.now()
-#    return render(request, "auditorias/auditorias.html", {"umbral":UserProfile.objects.all(), "date":Post.objects.all(), "now": datetime.datetime.now()})
-def porcentaje(lista1,keywords_2):
-    print(len(lista1),len(keywords_2))
-    porcentaje1 = 100*len(lista1)/len(keywords_2) #66.6%
-    print(porcentaje1)
-
-    if porcentaje1 > 65:
-        print("Similar")
-    else:
-        print("No similares")
-
-def auditorias(request):
-    lista = []
-    lista2= []
-    repetido = []
-
-    for x in Post.objects.all():
-        lista.append(x.title.upper())
-
-    keywords_2 = "buena los cabros".upper()
-
-    key3 = keywords_2.split(" ")
-
-    for i in lista:
-        lista2.append(i.split(" "))
-
-        
-    #volver la lista repetidos a cero
-    #agregar el else cuando es una sola palabra
-    #mandar las weas a porcentaje antes de volver la lista a cero
-    for palubria in lista2:
-        print(len(palubria))
-        for v in key3:
-            if len(palubria) > 1:
-                for b in palubria:
-                    if v == b:
-                        repetido.append(b)
-                        
-    print("-------**")
-    print(repetido)
-
-#    for m in key3:
-#        print(m)
-#        if z == m:
-#            repetido.append(z)
-
-#    print (contador)
-#    print('\n')
-
-
-
-    #keywords_2 = ['buena los cabros']
-#    print(keywords_2)
-#    print(lista1)
-
-
-    return render(request, "auditorias/auditorias.html", {"umbral":UserProfile.objects.all(), "date":Post.objects.all(),"parametros_rep":repetido})
-
-
-    
-
-def auditorias(request):
-            
-    return render(request, "auditorias/auditorias.html", {"date":Post.objects.all()})
-
-def repetido(request):
-    lista = []
-    unico = []
-    repetido= []
-    contador = 0
-    for x in Post.objects.all():
-        lista.append(x.title)
-        for z in lista:
-            if z not in unico:
-                unico.append(z)
-            else:
-                if z not in repetido:
-                    repetido.append(z)
-                    contador += 1
-                    #return render(request, "auditorias/auditorias.html", {"otro":Post.objects.all()})
-            
-    print (contador)
-    print (repetido)
-    return render(request, "auditorias/prueba.html", {"parametros":contador})
-
-"""
--- minutas/views.py
+++ minutas/views.py
@@ -7,18 +7,31 @@ from minutas.models import *
 
 def minutas(request):
     pass
-    
+
+def crear_minuta(request):
+    if request.method == 'POST':
+        form = MinutasForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('crear_minuta')+"?ok")
+    else:
+        form = MinutasForm()
+    return render(request, "minutas/_minutas.html", {'form':form})
+
+"""   
 def crear_minuta(request):
     minutas_form = MinutasForm()
 
     if request.method == "POST":
         minutas_form = MinutasForm(data=request.POST)
+        form = 
         if minutas_form.is_valid():
             pregunta = Minute(thematic = minutas_form.cleaned_data['them'],
-                               description = minutas_form.cleaned_data['desc'],
+                                document= minutas_form.cleaned_data['docu'],
                                address = minutas_form.cleaned_data['addr'])
             pregunta.save()
             #Todo bien
             return redirect(reverse('crear_minuta')+"?ok")
 
     return render(request, "minutas/_minutas.html", {'form':minutas_form})
+"""
