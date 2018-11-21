894be64df1864bdfdc2de619d1321e656325333c
Tomás Sepúlveda <t.sepulvedacaroca@gmail.com>
2018-11-21 02:54:45 -0300

    Anexado con moodle

--- retro_auth/views.py
+++ retro_auth/views.py
@@ -1,22 +1,103 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from retro.models import Section, Student
 from retro_auth.models import UserProfile
-from retro.models import Section,Subject
-import datetime
-import ast
-import json
-import os
 import requests
 
-def auth_login(request):
-    # logout(request)
-    username = password = ''
+url = 'http://pte.webtic.cl/login/token.php'
+url_api = 'http://pte.webtic.cl/webservice/rest/server.php'
+service = 'moodle_mobile_app'
+token = '4ac83a19f20d183aa254af9b8299f3df'
+id_teacher_moodle = 3
+id_student_moodle = 5
 
+
+def auth_login(request):
+    logout(request)
+    if request.POST:
+        # response = requests.post(url, data={'username': request.POST['username'],
+        #                                     'password': request.POST['password'],
+        #                                     'service': service})
+        username = request.POST['username']
+        password = request.POST['password']
+        response = requests.post(url, data={'username': username,
+                                            'password': password,
+                                            'service': service})
+        response = response.json()
+        if 'token' in response:
+            response = requests.post(url_api, data={'wsfunction': 'core_user_get_users_by_field',
+                                                    'moodlewsrestformat': 'json',
+                                                    'wstoken': token,
+                                                    'field': 'username',
+                                                    'values[0]': [username]})
+            information_moodle = response.json()[0]
+            response = requests.post(url_api, data={'wsfunction': 'core_enrol_get_users_courses',
+                                                    'moodlewsrestformat': 'json',
+                                                    'wstoken': token,
+                                                    'userid': information_moodle['id']})
+            course = response.json()
+            for x in course:
+                if not Section.objects.filter(pk=x['id']).exists():
+                    response = requests.post(url_api, data={'wsfunction': 'core_enrol_get_enrolled_users',
+                                                            'wstoken': token,
+                                                            'moodlewsrestformat': 'json',
+                                                            'courseid': x['id']})
+                    response = response.json()
+                    teacher_section = 0
+                    student_section_list = []
+                    for i in response:
+                        if i['roles'][0]['roleid'] == id_student_moodle:
+                            try:
+                                user = User.objects.get(pk=i['id'])
+                                userprofile = user.userprofile
+                                if userprofile.is_student:
+                                    userprofile.is_student = True
+                                    userprofile.save()
+                            except User.DoesNotExist:
+                                user = User.objects.create_user(pk=i['id'],
+                                                                username=i['username'],
+                                                                password='defaultpassword',
+                                                                last_name=i['fullname'])
+                                userprofile = UserProfile.objects.create(user=user, is_student=True)
+                            student_section_list.append(userprofile.pk)
+                        else:
+                            try:
+                                user = User.objects.get(pk=i['id'])
+                                if user.userprofile.is_teacher:
+                                    user.userprofile.is_teacher = True
+                                    user.userprofile.save()
+                            except User.DoesNotExist:
+                                user = User.objects.create_user(pk=i['id'],
+                                                                username=i['username'],
+                                                                password='defaultpassword',
+                                                                last_name=i['fullname'])
+                                UserProfile.objects.create(user=user, is_student=False, is_teacher=True)
+                            teacher_section = user.userprofile
+                    section = Section.objects.create(pk=x['id'], nrc=x['fullname'], teacher=teacher_section)
+                    for z in student_section_list:
+                        section.addStudent(z)
+                    # section = Section.objects.create(pk=x['id'], teacher=user)
+                # for i in response:
+                #     try:
+                #         User.objects.get(pk=i['id'])
+                #     except User.DoesNotExist:
+                #         User.objects.create_user(pk=i['id'],
+                #                                  username=i['username'],
+                #                                  password='defaultpassword',
+                #                                  last_name=i['fullname'])
+                #     section.addStudent(i['id'])
+                #
+            user = User.objects.get(username=username)
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            messages.error(request, 'Usuario y contraseña invalidos. Intentelo nuevamente.')
+    username = password = ''
     # if request.POST:
     #     request.session["h"] = 0
     #     username = request.POST["username"]
@@ -315,8 +396,7 @@ def auth_login(request):
     #     else:
     #         messages.error(request, 'Usuario y contraseña invalidos. Intentelo nuevamente.')
     # data = {}
-    template_name = "login.html"
-    return render(request, template_name, {})
+    return render(request, 'login.html', {})
 
 
 def auth_logout(request):
